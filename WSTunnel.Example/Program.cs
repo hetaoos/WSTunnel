4b4e9a1a691f89e6838768f19b045c9ac7de1948
Null <hetaoos@gmail.com>
2022-04-18 15:21:49 +0800

    简化

--- WSTunnel.Example/Program.cs
+++ WSTunnel.Example/Program.cs
@@ -8,17 +8,7 @@ builder.Services.AddWSTunnel();
 
 var app = builder.Build();
 
-var webSocketOptions = new WebSocketOptions
-{
-    KeepAliveInterval = TimeSpan.FromMinutes(2)
-};
-
-app.UseWebSockets(webSocketOptions);
+app.UseWebSockets();
 app.MapGet("/ws", WSTunnelRequestDelegate.Request);
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
